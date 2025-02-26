61eb3e0fd6eb863c4fb9df5fa6af6498d8617bda
Matteo Rossi <90650095+rossi-matteo@users.noreply.github.com>
2025-02-27 00:01:38 +0100

    Initial Sketch/Structure

---
+++ DatastoreServer.cc
@@ -0,0 +1,133 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "DatastoreServer.h"
+
+DatastoreServer::DatastoreServer() {} //TODO?
+
+DatastoreServer::~DatastoreServer() {} //TODO?
+
+void DatastoreServer::initalize(){
+    serverId = par("serverId");
+    totalServers = par("totalServers");
+
+    for(int i = 0; i < totalServers; i++)
+        vectorClock[i] = 0;
+
+    updateIdCounter = 0;
+
+    EV << "Server[" << serverId << "] starting" << endl;
+}
+
+
+void DatastoreServer::handleMessage(cMessage *msg){
+
+    if(msg -> isSelfMessage()){
+        if(msg == heartbeatTimer) {
+            //sendHeartbeats();
+            scheduleAt(simTime() + par("messageDelay").value() * 10, heartbeatTimer);
+        }
+        return;
+    }
+
+    NetworkMessages *inboundMsg = check_and_cast<NetworkMessages *>(msg);
+
+    if(dynamic_cast<ReadRequestMsg *>(inboundMsg)){
+        handleRead(check_and_cast<ReadRequestMsg *>(inboundMsg));
+    }
+    else if(dynamic_cast<WriteRequestMsg *>(inboundMsg)){
+        handleWrite(check_and_cast<WriteRequestMsg *>(inboundMsg));
+    }
+    else if(dynamic_cast<UpdateRequestMsg *>(inboundMsg)){
+        handleRead(check_and_cast<UpdateRequestMsg *>(inboundMsg));
+    }
+    /* TODO All the other messages here
+    else if(dynamic_cast<XXXRequestMsg *>(inboundMsg)){
+        handleRead(check_and_cast<XXRequestMsg *>(inboundMsg));
+    }
+     */
+}
+
+void DatastoreServer::finish(){
+    EV << "Server[" << serverId << "]" << endl;
+
+    EV << "Final Datastore Status <Key, Value>" << endl;
+    for(auto tuple : store){
+        EV << tuple.first << ": " << tuple.second << endl;
+    }
+
+    EV << "Vector Clock: " << endl << "[";
+    for(int index = 0; i < vectorClock.size()-1; i++){
+        EV << vectorClock[i] << ", ";
+    }
+    EV << vectorClock[i] << " ]" << endl;
+}
+
+void DatastoreServer::handleRead(ReadRequestMsg *msg){
+
+}
+
+void DatastoreServer::handleWrite(WriteRequestMsg *msg){
+
+}
+
+void DatastoreServer::handleUpdate(UpdateRequestMsg *msg){
+
+}
+
+void DatastoreServer::applyUpdate(){
+
+}
+
+void DatastoreServer::sendUpdate(std::string key, int value){
+
+}
+
+void DatastoreServer::checkPendingUpdates(){
+
+}
+
+bool DatastoreServer::checkCausalDependencies(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ DatastoreServer.h
@@ -0,0 +1,89 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+
+#include <omnetpp.h>
+#include <string>
+#include <map>
+#include <vector>
+#include <queue>
+//#include "CausalKVStoreMsg_m.h"
+
+using namespace omnetpp;
+
+#ifndef DATASTORESERVER_H_
+#define DATASTORESERVER_H_
+
+class DatastoreServer {
+    private:
+        int serverId;
+        int totalServers;
+
+        //Key-Value Store
+        std::map<std::string, int> store;
+
+        //Vector Clocks
+        std::vector<int> vectorClock;
+
+        //Pending Updates
+        typedef struct {
+            int updateId; //Maybe useless
+            int sourceId;
+
+            std::vector<int> senderVectorClock;
+
+            std::string key;
+            int value;
+        }PendingUpdate;
+
+        std::vector<PendingUpdate> pendingUpdates;
+
+        /*
+         * std::vector<bool>areServersUp;
+         * */
+
+        int updateIdCounter;
+
+        //cMessage *heartbeatTimer;
+
+    public:
+        DatastoreServer();
+        virtual ~DatastoreServer();
+
+        //Standard Omnet++ methods to Override
+        virtual void initialize() override;
+        virtual void handleMessage() override;
+        virtual void finish() override;
+
+        void handleRead(ReadRequestMsg *msg);
+        void handleWrite(WriteRequestMsg *msg);
+        void handleUpdate(UpdateRequestMsg *msg);
+
+        void sendUpdate(std::string key, int value);
+
+        void applyUpdate();
+        void checkPendingUpdates();
+
+        bool checkCausalDependencies();
+
+        //TODO
+        /*
+         * void handleHeartbeat(HeartbeatMsg *msg);
+         * void sendHeartbeat();
+         *
+         * */
+};
+
+#endif /* DATASTORESERVER_H_ */
+
+Define_Module(DatastoreServer);
